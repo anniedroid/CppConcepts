2fb8b9cc3d0ec80348dcd35518ffc5ac6d476485
Annie <annniedroid@gmail.com>
2023-07-19 00:39:36 -0700

    Merge w main branch. Exlicit destructor call added in main.cpp

--- main.cpp
+++ main.cpp
@@ -46,5 +46,8 @@ int main() {
     ConcertTicket taylor_swift_ticket;
     ConcertTicket EDM_ticket;
 
+    EDM_ticket.~ConcertTicket(); // Calling destructor explicitly
+    cout << "This is the end of my program\n";
+
     return 0;
 }
